b099d7b1f09acebb8aa3fcd5c03db30d07cdf578
Zachary Dowd <zacharyd2796@zohomail.com>
2021-09-29 10:40:57 -0600

    final touches

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/styles.css",
   "/db.js",
+  "/images/screenshot.png",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "/manifest.webmanifest",
